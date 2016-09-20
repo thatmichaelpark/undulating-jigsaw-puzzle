3093265201aeb5f51838f9c16db26fd8d53b8c79
Michael Park <thatmichaelpark@gmail.com>
2016-09-20 06:45:31 -0700

    signup dialog

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -1,8 +1,43 @@
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import Joi from 'joi';
 import React from 'react';
+import TextField from 'material-ui/TextField';
+
+const schema = Joi.object({
+  username: Joi.string().trim().max(20),
+  password: Joi.string().trim().max(50),
+  confirmPassword:
+  Joi.any().valid(Joi.ref('password')).options({ language: { any: { allowOnly: 'must match password' } } })
+});
 
 const Signup = React.createClass({
+  getInitialState() {
+    return {
+      errors: {},
+      username: '',
+      password: '',
+      confirmPassword: ''
+    };
+  },
+  handleBlur(event) {
+    const { name, value } = event.target;
+    const nextErrors = Object.assign({}, this.state.errors);
+    const result = Joi.validate({ [name]: value }, schema);
+
+    if (result.error) {
+      for (const detail of result.error.details) {
+        nextErrors[detail.path] = detail.message;
+      }
+      return this.setState({ errors: nextErrors });
+    }
+    delete nextErrors[name];
+    this.setState({ errors: nextErrors });
+  },
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  },
   render() {
     const signupActions = [
       <FlatButton
@@ -13,18 +48,49 @@ const Signup = React.createClass({
       <FlatButton
         label="OK"
         primary={true}
-        keyboardFocused={true}
         onTouchTap={this.props.handleOk}
       />,
     ];
+
+    const { errors, username, password, confirmPassword } = this.state;
+
     return (
       <Dialog
-        title="Signup Dialog"
+        title="Sign up to keep track of your solved puzzles"
         actions={signupActions}
         modal={true}
         open={this.props.open}
       >
-        Signup stuff goes here...
+        <TextField
+          autoFocus
+          errorText={errors.username}
+          floatingLabelText="Username"
+          fullWidth={true}
+          name='username'
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+          value={username}
+        />
+        <TextField
+          errorText={errors.password}
+          floatingLabelText="Password"
+          fullWidth={true}
+          name='password'
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+          type="password"
+          value={password}
+        />
+        <TextField
+          errorText={errors.confirmPassword}
+          floatingLabelText="Confirm password"
+          fullWidth={true}
+          name='confirmPassword'
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+          type="password"
+          value={confirmPassword}
+        />
       </Dialog>
     );
   }
