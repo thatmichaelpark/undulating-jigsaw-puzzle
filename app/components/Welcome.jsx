4345fab70ca4acb1a9ae0a95a105ac664693108d
Michael Park <thatmichaelpark@gmail.com>
2016-09-20 21:13:21 -0700

    started welcome screen

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -7,9 +7,25 @@ const Welcome = React.createClass({
   },
   render() {
     return (
-      <div>
-        Welcome to blah blah blah...
-        <RaisedButton label="Start" onTouchTap={this.handleStartTouchTap} />
+      <div className='welcome'>
+        <div className='welcome-copy'>
+          Do you like jigsaw puzzles? Me neither. Try something different: puzzles with
+          moving edges
+        </div>
+        <div className="welcome-columns">
+          <div className="welcome-column">
+            Animated wavy edges.
+          </div>
+          <div className="welcome-column">
+            Rotate pieces.
+          </div>
+          <div className="welcome-column">
+            Lorem ipsum.
+          </div>
+        </div>
+        <div className="welcome-button">
+          <RaisedButton label="Start" onTouchTap={this.handleStartTouchTap} primary={true}/>
+        </div>
       </div>
     );
   }
