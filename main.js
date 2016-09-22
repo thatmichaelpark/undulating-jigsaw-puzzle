50e301ceba69a60fccbbc8c21307d564e5e38aeb
Michael Park <thatmichaelpark@gmail.com>
2016-09-22 11:28:56 -0700

    begin electron stuff

---
+++ main.js
@@ -0,0 +1,52 @@
+// electron main
+
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow();
+  mainWindow.loadURL(`http://localhost:8000/`);
+
+  const name = app.getName();
+  const template = [
+    {
+      label: name,
+      submenu: [{
+        label: `About ${name}`,
+        role: 'about'
+      }, {
+        type: 'separator'
+      }, {
+        label: `Hide ${name}`,
+        role: 'hide',
+        accelerator: 'Cmd+H'
+      }, {
+        label: `Hide Others`,
+        role: 'hideothers',
+        accelerator: 'Alt+Cmd+H'
+      }, {
+        label: `Show All`,
+        role: 'unhide'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Quit',
+        click: app.quit,
+        accelerator: 'Cmd+Q'
+      }]
+    }
+  ];
+
+  const menu =  Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  mainWindow.on('closed', () => {
+    console.log('closed');
+    mainWindow = null;
+  });
+});
