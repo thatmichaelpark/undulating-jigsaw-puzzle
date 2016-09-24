bb675c1b4c9b3ac0dd6345e0652768a081d4a4c9
Michael Park <thatmichaelpark@gmail.com>
2016-09-23 23:30:42 -0700

    linted welcome

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -1,9 +1,9 @@
+import { Card, CardText } from 'material-ui/Card';
 import Nav from 'components/Nav';
+import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import { withRouter } from 'react-router';
-import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
-import Paper from 'material-ui/Paper';
 
 const Welcome = React.createClass({
   handleStartTouchTap() {
@@ -25,58 +25,69 @@ const Welcome = React.createClass({
         padding: '2rem'
       }
     };
+
     return (
       <div>
         <Nav />
-        <div style={{
-          backgroundImage: 'url(/images/27_haruhi_jigsaw_03.jpg)',
-          position: 'absolute',
-          top: '0',
-          left: '0',
-          right: '0',
-          bottom: '0'
-        }}>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'rgba(255, 255, 255, 0.7)',
-            height: '100%'
-          }}>
+        <div
+          style={{
+            backgroundImage: 'url(/images/27_haruhi_jigsaw_03.jpg)',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0'
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'rgba(255, 255, 255, 0.7)',
+              height: '100%'
+            }}
+          >
             <Paper style={styles.paper} zDepth={0}>
               <p>
-              An experimental variation on traditional jigsaw puzzles. The pieces
-              of these puzzles have borders that vary over time. Different puzzles
-              vary at different rates.
+              An experimental variation on traditional jigsaw puzzles.
+              The pieces of these puzzles have borders that vary over time.
+              Different puzzles' borders vary at different rates.
               </p>
-              <div style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center'
-              }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  justifyContent: 'center'
+                }}
+              >
                 <Card style={styles.card} zDepth={0}>
                   <CardText>
-                    Left-click and drag pieces; if a piece is placed next to a piece
-                    with a match border, the pieces will click together.
+                    Left-click and drag pieces; if a piece is placed next
+                    to a piece with a matching border, the pieces will
+                    click together.
                   </CardText>
                 </Card>
                 <Card style={styles.card} zDepth={0}>
                   <CardText>
-                    Some puzzles require you to rotate pieces. Right-click a piece to rotate it clockwise 90°; to rotate counterclockwise, right-click while holding down the shift key.
+                    Some puzzles require you to rotate pieces. Right-click
+                    a piece to rotate it clockwise 90°; to rotate
+                    counterclockwise, right-click while holding down
+                    the shift key.
                   </CardText>
                 </Card>
                 <Card style={styles.card} zDepth={0}>
                   <CardText>
-                    You don't <em>have</em> to log in, but if you do, your puzzle times will be
-                    recorded.
+                    You don't <em>have</em> to log in, but if you do,
+                    your puzzle times will be recorded.
                   </CardText>
                 </Card>
               </div>
               <RaisedButton
-              label="Start"
-              onTouchTap={this.handleStartTouchTap}
-              primary={true}
+                label="Start"
+                onTouchTap={this.handleStartTouchTap}
+                primary={true}
               />
             </Paper>
           </div>
