642ff8ac595b53d505ec362880821cdd16e54cca
Michael Park <thatmichaelpark@gmail.com>
2016-09-25 15:09:38 -0700

    test: puzzles

--- test/server.test.js
+++ test/server.test.js
@@ -115,3 +115,145 @@ suite('users routes', () => {
       });
   });
 });
+
+suite('puzzles routes', () => {
+  before((done) => { // eslint-disable-line no-undef
+    knex.migrate.latest()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+  beforeEach((done) => { // eslint-disable-line no-undef
+    knex.seed.run()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+  test('GET /api/puzzles', (done) => {
+    supertest(app)
+      .get('/api/puzzles')
+      .set('Accept', 'application/json, */*')
+      .expect('Content-Type', /json/)
+      .expect((result) => {
+        const puzzles = result.body;
+        console.log('puzzles:', puzzles);
+        puzzles.forEach((puzzle) => {
+          delete puzzle.createdAt;
+          delete puzzle.updatedAt;
+        });
+      })
+      .expect(200, [
+          {
+              backgroundColor: "#db9276",
+              hasRotatedPieces: false,
+              id: 1,
+              imageUrl: "/images/seattle.jpg",
+              maxFreq: 2,
+              maxV: 0.5,
+              maxWaveDepth: 20,
+              nCols: 3,
+              nRows: 2,
+              nWaves: 3,
+              pieceContentSize: 200,
+          },
+          {
+              backgroundColor: "#5979AF",
+              hasRotatedPieces: false,
+              id: 2,
+              imageUrl: "/images/clouds-06.jpg",
+              maxFreq: 5,
+              maxV: 0.5,
+              maxWaveDepth: 15,
+              nCols: 4,
+              nRows: 3,
+              nWaves: 3,
+              pieceContentSize: 150,
+          },
+          {
+              backgroundColor: "white",
+              hasRotatedPieces: false,
+              id: 3,
+              imageUrl: "/images/cat800.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 20,
+              nCols: 4,
+              nRows: 4,
+              nWaves: 2,
+              pieceContentSize: 150,
+          },
+          {
+              backgroundColor: "#D8780A",
+              hasRotatedPieces: true,
+              id: 4,
+              imageUrl: "/images/fractal-026.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 20,
+              nCols: 5,
+              nRows: 3,
+              nWaves: 2,
+              pieceContentSize: 150,
+          },
+          {
+              backgroundColor: "#F2B5B5",
+              hasRotatedPieces: true,
+              id: 5,
+              imageUrl: "/images/JellyBellyBeans.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 20,
+              nCols: 5,
+              nRows: 4,
+              nWaves: 2,
+              pieceContentSize: 120,
+          },
+          {
+              backgroundColor: "#0F74FF",
+              hasRotatedPieces: true,
+              id: 6,
+              imageUrl: "/images/jellyfish.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 20,
+              nCols: 6,
+              nRows: 4,
+              nWaves: 2,
+              pieceContentSize: 120,
+          },
+          {
+              backgroundColor: "#EDD2AC",
+              hasRotatedPieces: true,
+              id: 7,
+              imageUrl: "/images/fractal-1119594_960_720.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 20,
+              nCols: 9,
+              nRows: 5,
+              nWaves: 3,
+              pieceContentSize: 100,
+          },
+          {
+              backgroundColor: "black",
+              hasRotatedPieces: true,
+              id: 8,
+              imageUrl: "/images/black.jpg",
+              maxFreq: 3,
+              maxV: 1,
+              maxWaveDepth: 10,
+              nCols: 9,
+              nRows: 9,
+              nWaves: 3,
+              pieceContentSize: 50,
+          }
+      ])
+      .end(done);
+  });
+});
