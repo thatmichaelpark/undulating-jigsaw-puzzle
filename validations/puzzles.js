ae94ea5656bd3c8b48543c5c3c42da35cd4ff64d
Michael Park <thatmichaelpark@gmail.com>
2016-09-29 22:59:06 -0700

    added puzzles route validations

--- validations/puzzles.js
+++ validations/puzzles.js
@@ -5,12 +5,30 @@ const Joi = require('joi');
 module.exports = {
   post : {
     body: {
-      // nRows: Joi.number().min(2).max(10),
-      // nCols: Joi.number().min(2).max(10),
+      imageUrl: Joi.string().required(),
+      backgroundColor: Joi.string().required(),
+      nRows: Joi.number().integer().min(2).max(10).required(),
+      nCols: Joi.number().integer().min(2).max(10).required(),
+      pieceContentSize: Joi.number().integer().min(1).required(),
+      hasRotatedPieces: Joi.boolean().required(),
+      nWaves: Joi.number().integer().min(0).max(5).required(),
+      maxWaveDepth: Joi.number().min(0).required(),
+      maxFreq: Joi.number().min(0).required(),
+      maxV: Joi.number().min(0).required()
     }
   },
   patch : {
     body: {
+      imageUrl: Joi.string().required(),
+      backgroundColor: Joi.string().required(),
+      nRows: Joi.number().integer().min(2).max(10).required(),
+      nCols: Joi.number().integer().min(2).max(10).required(),
+      pieceContentSize: Joi.number().integer().min(1).required(),
+      hasRotatedPieces: Joi.boolean().required(),
+      nWaves: Joi.number().integer().min(0).max(5).required(),
+      maxWaveDepth: Joi.number().min(0).required(),
+      maxFreq: Joi.number().min(0).required(),
+      maxV: Joi.number().min(0).required()
     }
   },
 };
